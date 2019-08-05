21a425b0a39e4e8c65b793f774391a9c2744e676
Luis <luchopintado@gmail.com>
2019-08-05 11:38:59 -0500

    refactor(user-form): wrap into module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { UserFormComponent } from './user-form/user-form.component';
-import { UserSummaryComponent } from './user-form/user-summary/user-summary.component';
-import { UserDetailsComponent } from './user-form/user-details/user-details.component';
-import { AccountDetailsComponent } from './user-form/account-details/account-details.component';
-
-import { MaterialModule } from './material.module';
-import { SummaryService } from './user-form/user-details/summary.service';
+import { UserFormModule } from './user-form/user-form.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserFormComponent,
-    UserSummaryComponent,
-    UserDetailsComponent,
-    AccountDetailsComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    ReactiveFormsModule,
+    UserFormModule,
   ],
-  providers: [
-    SummaryService,
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user-form/user-form.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material.module';
+
+import { UserFormComponent } from './user-form.component';
+import { UserSummaryComponent } from './user-summary/user-summary.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { AccountDetailsComponent } from './account-details/account-details.component';
+
+import { SummaryService } from './user-details/summary.service';
+
+@NgModule({
+    declarations: [
+        UserFormComponent,
+        UserSummaryComponent,
+        UserDetailsComponent,
+        AccountDetailsComponent,
+    ],
+    imports: [
+        CommonModule,
+        MaterialModule,
+        ReactiveFormsModule,
+    ],
+    exports: [
+        UserFormComponent,
+    ],
+    providers: [
+        SummaryService,
+    ],
+})
+export class UserFormModule {}
