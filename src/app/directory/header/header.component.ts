e07822042c8801236421dc5ddbb3ac1669157d70
Luis <luchopintado@gmail.com>
2019-08-05 21:27:17 -0500

    fix(header): style and back functionality

--- src/app/directory/header/header.component.ts
+++ src/app/directory/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
@@ -7,8 +9,15 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
     @Input() title = '';
+    showBack = true;
 
-    constructor() { }
+    constructor(location: Location, router: Router) {
+        router.events.subscribe((obj: any) => {
+            if (obj.url === '/directory') {
+                this.showBack = false;
+            }
+        });
+    }
 
     ngOnInit() {
     }
